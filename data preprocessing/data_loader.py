be8105dcdd01ff6bd5862cf145afafb1d3cea4b0
tatiana-boura <tat.boura@gmail.com>
2023-04-10 17:35:44 +0300

    Dataset - PyTorch format

--- data preprocessing/data_loader.py
+++ data preprocessing/data_loader.py
@@ -1,6 +1,10 @@
 import os
 import re
 import pandas as pd
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
 
 '''
 This code reads the CNF clauses in the given format and converts them to a format that will be useful for constructing the graph
@@ -20,7 +24,7 @@ the AND operator. The number of row is the number of clauses .
 '''
 
 # create columns of dataset
-dict = {"numberOfVariables": [],"numberOfClauses": [], "variablesSymb": [], "variablesNum": [], "edges" : [], "label" : []}
+dict = {"numberOfVariables": [],"numberOfClauses": [], "variablesSymb": [], "variablesNum": [], "edges" : [], "edgeAttr" : [], "label" : []}
 # create dataframe
 df = pd.DataFrame(dict)
 
@@ -33,7 +37,8 @@ for dirName in os.listdir(directory):
         dirInfo = dirName.split(".")
         # number of variables in each data-file regarding the folder
         numberOfVariables = int((re.findall(r'\d+',dirInfo[0]))[0])
-        # number of clauses in each data-file regarding the folder
+        # number of clauses in each
+        # data-file regarding the folder
         numberOfClauses = int(dirInfo[1])
         # get label of these data : UUF means UNSAT and UF means SAT
         y = 0 if dirInfo[0][:3] == "UUF" else 1
@@ -45,9 +50,18 @@ for dirName in os.listdir(directory):
 
         nodes = [i for i in range(0, 2 * numberOfVariables + numberOfClauses)]
         # node with symbolic values
+        '''
         node_values = ["x" + str(i + 1) for i in range(0, numberOfVariables)]
         node_values += ["-x" + str(i + 1) for i in range(0, numberOfVariables)]
         node_values += ["c" + str(i + 1) for i in range(0, numberOfClauses)]
+        '''
+        '''
+        node_values = [i+1 for i in range(0, numberOfVariables)]
+        node_values += [-(i+1) for i in range(0, numberOfVariables)]
+        node_values += [numberOfVariables for i in range(0, numberOfClauses)]
+        '''
+
+        node_values = [[np.random.uniform()] for _ in range(0, 2 * numberOfVariables + numberOfClauses)]
 
         for fileName in os.listdir(currDir):
             f = open(currDir + "/" + fileName, "r")
@@ -56,43 +70,90 @@ for dirName in os.listdir(directory):
             clauses = [line[:-2] for line in clauses]     # keep only the corresponding variables
             clauses = [line for line in clauses if line != '']  # keep only the lines that correspond to a clause
 
+            '''
             if len(clauses) != numberOfClauses:
                 # if the lines(clauses) that we processed are not the correct number(), raise Exception
                 raise Exception("Something went wrong with the line processing")
+            '''
 
+            # edges
             edges_1 = []
             edges_2 = []
 
+            # compute edge attributes as x_i -> ~x_i are
+            # connected via a different edge than c_j and x_i
+            edgeAttr = []
+
             # make the edges from x_i -> ~x_i and ~x_i -> x_i
             for i in range(numberOfVariables):
-                edges_1 += [i]
-                edges_1 += [i + numberOfVariables]
 
-                edges_2 += [i + numberOfVariables]
+                temp = [i + numberOfVariables]
+                edges_1 += [i]
+                edges_1 += temp
+                edges_2 += temp
                 edges_2 += [i]
 
-            # make the edges from corresponding c_j -> x_i (NOT VICE VERSA)
+                # first characteristic is :  connection between x_i and ~x_i
+                # second characteristic is :  connection between c_j and x_i
+                edgeAttr += [[1, 0]]
+                '''
+                # third characteristic is :  connection between c_j and c_k
+                edgeAttr += [[1,0,0]]
+                '''
+
+            # make the edges from corresponding c_j -> x_i (NOW VICE VERSA)
             count = 0
             for clause in clauses:
                 clauseVars = clause.split(" ")
                 clauseVars = [int(var) for var in clauseVars]
                 # create the corresponding edges
                 for xi in clauseVars:
+                    temp = [xi-1] if xi>0 else [abs(xi)-1+numberOfVariables]
                     edges_1 += [count + 2*numberOfVariables]
-                    edges_1 += [xi-1] if xi>0 else [abs(xi)-1+numberOfVariables]
+                    edges_1 += temp
+                    edges_2 += temp
+                    edges_2 += [count + 2*numberOfVariables]
+
+                    edgeAttr += [[0,1]]
+                    #edgeAttr += [[0, 1, 0]]
 
                 count += 1
             '''
+            # make the edges from c_j -> c_k
+            for i in range(numberOfClauses-1):
+                clausesPos = 2*numberOfVariables+i
+
+                #temp = [clausesPos+1 for _ in range(0,numberOfClauses-i-1)]
+                for j in range(1, numberOfClauses - i):
+                    edges_1 += [clausesPos]
+                    edges_1 += [clausesPos+j]
+
+                    edges_2 += [clausesPos+j]
+                    edges_2 += [clausesPos]
+
+                    edgeAttr += [[0, 0, 1]]
+            '''
+            '''
+            totalNumOfEdges = 3*numberOfClauses +
+            if len(edgeAttr) != numberOfClauses:
+                # if the lines(clauses) that we processed are not the correct number(), raise Exception
+                raise Exception("Something went wrong with the line processing")
+            '''
+
+
             if fileName == "uf4.cnf":
                 print(nodes)
                 print(node_values)
                 print(clauses)
                 print(edges_1)
                 print(edges_2)
-            '''
+
             f.close()
 
-            # ready to insert new row in dataframe : "numberOfVariables","numberOfClauses", "variablesSymb", "variablesNum", "edges", "label"
-            df.loc[len(df)] = [numberOfVariables, numberOfClauses, node_values, nodes, (edges_1,edges_2), y]
+            # ready to insert new row in dataframe : "numberOfVariables","numberOfClauses", "variablesSymb", "variablesNum", "edges", "edgeAttr","label"
+            df.loc[len(df)] = [numberOfVariables, numberOfClauses, node_values, nodes, [edges_1,edges_2], [edgeAttr,edgeAttr], [y]]
 
-df.to_csv("dataset.csv",index=False)
+
+store = pd.HDFStore('store.h5')
+store['df'] = df
+#df.to_csv("./dataset.csv",index=False)
---
+++ data preprocessing/dataset_pytorch.py
@@ -0,0 +1,49 @@
+from torch_geometric.data import Dataset, Data
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import torch
+import os
+
+class SAT3Dataset(Dataset):
+    def __init__(self, root, filename, transform=None, pre_transform=None):
+        self.filename = filename
+        super(SAT3Dataset, self).__init__(root, transform, pre_transform)
+
+    @property
+    def raw_file_names(self):
+        return self.filename
+
+    @property
+    def processed_file_names(self):
+        return "no_implemented_stuff.pt"
+
+    def download(self):
+        # there won't be any need to download the data
+        pass
+
+    def process(self):
+        # open the dataframe
+        store = pd.HDFStore(self.filename)
+        self.data = store['df']
+
+        for index, cnf in tqdm(self.data.iterrows(), total=self.data.shape[0]):
+            # get node features (here we actually don't have many)
+            node_feats = torch.tensor(cnf["variablesSymb"], dtype=torch.float)
+            # get adjacency info
+            edge_index = torch.tensor(cnf["edges"], dtype=torch.long)
+            num_edges = edge_index.size(dim=1)
+            # get edge features |  view is used in order to get the correct dimensions as specified by COO format
+            edge_feats = torch.tensor(cnf["edgeAttr"], dtype=torch.float).view(num_edges, -1)
+            # get labels info
+            label = torch.tensor(np.asarray(cnf["label"]), dtype=torch.int64)
+            # now, create data object
+            data = Data(x=node_feats, edge_index=edge_index, edge_attr=edge_feats, y=label)
+            # save the data
+            torch.save(data,os.path.join(self.processed_dir,f'data_{index}.pt'))
+
+    def len(self):
+        return self.data.shape[0]
+
+    def get(self, idx):
+        return torch.load(os.path.join(self.processed_dir, f'data_{idx}.pt'))
